e355c818f7c4b14d7ebe1c3623be1c19a8f57681
BulBal <lss523012@gmail.com>
2025-03-28 21:59:43 +0900

    tmp

--- client/Metamong/Assets/Scripts/WriteFileTest.cs
+++ client/Metamong/Assets/Scripts/WriteFileTest.cs
@@ -7,68 +7,89 @@ using Whisper.Utils;
 using System;
 using System.Text;
 using System.Reflection;
+using Whisper;
+using System.Threading.Tasks;
+using System.Linq;
 
 
-public class WriteFileTest : MonoBehaviour
-{
-    public Text text;
-    public Button button;
-
-    public int count = 0;
-
-    public GameObject targetObject; // 대상 GameObject를 Inspector에서 지정
-    public string componentTypeName = "WhisperManager"; // 컴포넌트 클래스 이름
-
-    private readonly string[] targetFieldNames = { "stepSec", "keepSec", "lengthSec" };
-
-    void Start()
+    
+    public class WriteFileTest : MonoBehaviour
     {
-         
-       
-        button.onClick.AddListener(WriteFile);
-    }
-    private void WriteFile()
-    {
-        if(count == 1)
+        // private WhisperWrapper whisperWrapper;
+        public Text text;
+        public Button button;
+
+        public int count = 0;
+
+        public GameObject targetObject;
+
+
+        private readonly string[] targetFieldNames = { "stepSec", "keepSec", "lengthSec" };
+
+        private WhisperWrapper wrapper;
+        private List<long> infer = new List<long>();
+        void Start()
         {
-            Debug.Log("Writing to file...  ");
-            string result = text.text;
-            if (targetObject == null)
-            {
-                Debug.LogWarning("⚠ targetObject가 비어있습니다!");
-                return;
-            }
-
-            Component comp = targetObject.GetComponent(componentTypeName);
-            StringBuilder sb = new StringBuilder();
-            
-            if (comp == null)
-            {
-                Debug.LogWarning($"⚠ 컴포넌트 '{componentTypeName}' 을(를) 찾을 수 없습니다!");
-                return;
-            }
-
-            Type type = comp.GetType();
-
-            foreach (string fieldName in targetFieldNames)
-            {
-                FieldInfo field = type.GetField(fieldName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-                if (field != null)
-                {
-                    object value = field.GetValue(comp);
-                    sb.AppendLine($"{fieldName} = {value}");
-                    
-                }
-                else
-                {
-                    Debug.LogWarning($"⚠ 필드 {fieldName} 를 찾을 수 없습니다!");
-                }
-            }
-            sb.AppendLine(result);
-            
-            result = sb.ToString();
-            File.WriteAllTextAsync("full_sentence.txt", result);
+            button.onClick.AddListener(WriteFile);
         }
-        count++;
+        private void WriteFile()
+        {
+            
+            
+            if(count == 1)
+            {
+                Debug.Log("Writing to file...  ");
+                string result = text.text;
+                
+                if (targetObject == null)
+                {
+                    Debug.LogWarning("⚠ targetObject가 비어있습니다!");
+                    return;
+                }
+
+                WhisperManager wm = targetObject.GetComponent<WhisperManager>();
+                
+                if(wm == null)
+                {
+                    Debug.LogWarning("⚠ whisperManager 컴포넌트를 찾을 수 없습니다!");
+                    return;
+                }
+
+                // Component comp = targetObject.GetComponent(wmType);
+                // StringBuilder sb = new StringBuilder();
+
+                Type type = wm.GetType();
+                StringBuilder sb = new StringBuilder();
+
+                foreach (string fieldName in targetFieldNames)
+                {
+                    FieldInfo field = type.GetField(fieldName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                    if (field != null)
+                    {
+                        object value = field.GetValue(wm);
+                        sb.AppendLine($"{fieldName} = {value}");
+                    }
+                    else
+                    {
+                        Debug.LogWarning($"⚠ 필드 {fieldName} 를 찾을 수 없습니다!");
+                    }
+                }
+            
+                sb.AppendLine(result);
+                
+                
+                infer = wrapper.GetInferenceTime();
+                Debug.Log(string.Join(",", infer));
+
+                double avg = infer.Average();
+                
+                result = sb.ToString() + $"\n avg = {avg:F2}";
+
+                File.AppendAllText("full_sentence.txt", result);
+                
+            }
+            count++;
+        }
     }
-}
+    
+
